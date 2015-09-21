d979efeb249143b7ce944199db69040d9c543bfa
nifferdil <nifferdil@gmail.com>
2015-09-21 12:11:00 -0700

    add files

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $("form#new-place").submit(function(event) {
+      event.preventDefault();
+
+      var inputtedCity = $("input#new-city").val();
+      var inputtedCountry = $("input#new-country").val();
+      var inputtedYear = $("input.new-year").val();
+      var inputtedPerson = $("input.new-person").val();
+      var inputtedNotes = $("input.new-notes").val();
+
+      var newPlace = { city: inputtedCity,
+                       country: inputtedCountry,
+                       year: inputtedYear,
+                       person: inputtedPerson,
+                       notes: inputtedNotes };
+
+      $("ul#places").append("<li><span class='place'>" + newPlace.city + ", " + newPlace.country + "</span></li>");
+
+        $(".place").last().click(function() {
+        $("#show-place").show();
+        $("#show-place h2").text(newPlace.city + ", " + newPlace.country);
+        $(".new-city").text(newPlace.city);
+        $(".new-country").text(newPlace.country);
+        $(".new-year").text(newPlace.year);
+        $(".new-person").text(newPlace.person);
+        $(".new-notes").text(newPlace.notes);
+
+      });
+
+      $("input#new-city").val("");
+      $("input#new-country").val("");
+      $("input.new-year").val("");
+      $("input.new-person").val("");
+      $("input.new-notes").val("");
+    });
+  });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+// describe('bottlesOfBeer', function() {
+//   it("counts down one number from the user inputted number", function() {
+//     expect(bottlesOfBeer(3)).to.equal("3 bottles of beer on the wall. 3 bottles of beer. Take one down pass it around- 2 bottles of beer on the wall. 2 bottles of beer on the wall. 2 bottles of beer. Take one down pass it around- 1 bottles of beer on the wall. 1 bottles of beer on the wall. 1 bottles of beer. Take one down pass it around- 0 bottles of beer on the wall. No more bottles of beer on the wall no more bottles of beer. Go to the store and buy some more 99 bottles of beer on the wall.");
+//   });
+// });
