921c816524f9b4b9875bbab1f983295caa2be891
nifferdil <nifferdil@gmail.com>
2015-09-21 14:09:02 -0700

    toggle adding new address

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
 
-    $("form#new-place").submit(function(event) {
-      event.preventDefault();
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
 
-      var inputtedCity = $("input#new-city").val();
-      var inputtedCountry = $("input#new-country").val();
-      var inputtedYear = $("input.new-year").val();
-      var inputtedPerson = $("input.new-person").val();
-      var inputtedNotes = $("input.new-notes").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedCountry = $("input#new-country").val();
+    var inputtedYear = $("input.new-year").val();
+    var inputtedPerson = $("input.new-person").val();
+    var inputtedNotes = $("input.new-notes").val();
 
-      var newPlace = { city: inputtedCity,
-                       country: inputtedCountry,
-                       year: inputtedYear,
-                       person: inputtedPerson,
-                       notes: inputtedNotes };
+    var newPlace = { city: inputtedCity,
+      country: inputtedCountry,
+      year: inputtedYear,
+      person: inputtedPerson,
+      notes: inputtedNotes };
 
       $("ul#places").append("<li><span class='place'>" + newPlace.city + ", " + newPlace.country + "</span></li>");
 
-        $(".place").last().click(function() {
+      $(".place").last().click(function() {
         $("#show-place").show();
         $("#show-place h2").text(newPlace.city + ", " + newPlace.country);
         $(".new-city").text(newPlace.city);
