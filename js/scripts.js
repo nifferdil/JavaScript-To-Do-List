0fcebdab3f23992d96dc1aae0d654cfe91faa1f1
nifferdil <nifferdil@gmail.com>
2015-09-21 16:56:33 -0700

    mark task as complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
 
   $("form#new-task").submit(function(event) {
-  //  var inputtedTask = $("input#new-task").val();
     var inputtedTask = $(this).find("input#new-task").val();
 
     $("ul#tasks").append("<li><span class='task'>" + inputtedTask + "</span></li>");
 
 
-    $(".task").click(function() {
-
-      $("ul#complete").append("<li><span class='task'>" + inputtedTask + "</span></li>");
-      $(this).remove();
-
+    $(".task").last().click(function() {
+      $("#tasks li").last().appendTo('#complete');
     });
 
-  $("input#new-task").val("");
-  event.preventDefault();
-});
+    $("input#new-task").val("");
+    event.preventDefault();
+  });
 
 });
